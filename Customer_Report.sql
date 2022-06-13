5c0eedc2e57f27ed4efb4170bae4a85cd6fe3f93
cristianacmce <cristianacmc@gmail.com>
2022-06-13 20:47:25 +0100

    Replaced Cudstomers Report

--- Customer_Report.sql
+++ Customer_Report.sql
@@ -1,27 +1,21 @@
+/*Customers Report*/
 
-
-SELECT pp.[BusinessEntityID]
+SELECT pp.BusinessEntityID AS PersonID
       ,pp.[FirstName]
       ,pp.[LastName]
 	  ,pp.FirstName + ' ' + pp.LastName AS [Full Name]
-	  ,a.City
+	  ,pa.City
 	  --,a.StateProvinceID
-	  ,sp.CountryRegionCode AS CountryCode
+	  --,sp.CountryRegionCode AS CountryCode
 	  --,sp.TerritoryID
-	  ,st.name AS Territory
-	  ,st.[Group] AS TerritoryGroup
-  FROM [Person].[Person] AS pp
-  INNER JOIN Person.BusinessEntity AS be
-	ON pp.BusinessEntityID = be.BusinessEntityID
-  INNER JOIN Person.BusinessEntityAddress AS bea
-	ON be.BusinessEntityID = bea.BusinessEntityID
-  INNER JOIN Person.Address AS a
-	ON a.AddressID = bea.AddressID
-  INNER JOIN Person.StateProvince AS sp
-	ON a.StateProvinceID = sp.StateProvinceID
-  INNER JOIN sales.SalesTerritory st
-	ON sp.TerritoryID = st.TerritoryID
-ORDER BY st.[Group]
+	  --,st.name AS Territory
+	  --,st.[Group] AS TerritoryGroup
+  FROM Person.Person AS pp
+  INNER JOIN Person.BusinessEntityAddress ba
+	ON pp.BusinessEntityID = ba.BusinessEntityID
+  INNER JOIN Person.Address AS pa
+	ON ba.AddressID = pa.AddressID
+
 
 
  
